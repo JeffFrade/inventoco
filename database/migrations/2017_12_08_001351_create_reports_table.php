b5daabd8afb89d86698b7a89d3a22055a1263c1c
Jefferson Frade <jefferrson.frade@gmail.com>
2017-12-07 22:29:54 -0200

    Created the migrations

---
+++ database/migrations/2017_12_08_001351_create_reports_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->string('storage', 2000);
+            $table->string('user');
+            $table->string('serial_number');
+            $table->timestamps();
+            $table->foreign('user')
+                ->references('user')
+                ->on('user')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreign('serial_number')
+                ->references('serial_number')
+                ->on('equipments')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
---
+++ database/migrations/2017_12_08_002156_create_occurrences_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOccurrencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('occurrences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 50);
+            $table->string('description', 2000);
+            $table->string('serial_number');
+            $table->integer('id_criticality')->unsigned();
+            $table->integer('id_type')->unsigned();
+            $table->string('user');
+            $table->timestamps();
+            $table->foreign('serial_number')
+                ->references('serial_number')
+                ->on('equipments')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreign('id_criticality')
+                ->references('id_criticality')
+                ->on('criticalities')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreign('id_type')
+                ->references('id_type')
+                ->on('type_occurrences')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreign('user')
+                ->references('user')
+                ->on('user')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('occurrences');
+    }
+}
