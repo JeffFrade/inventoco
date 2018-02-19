fe2d2ece805ed96c61f6699183edd643a8aa782c
Jefferson Frade <jefferrson.frade@gmail.com>
2018-02-18 23:11:05 -0300

    Chart Provider Service Has Been Altered to Support

--- app/Http/Services/Charts.php
+++ app/Http/Services/Charts.php
@@ -4,31 +4,29 @@ namespace InvOco\Http\Services;
 
 use InvOco\Repositories\EquipmentsRepository;
 use InvOco\Repositories\OccurrencesRepository;
-use InvOco\Services\Chart;
+use InvOco\Support\Chart as SupChart;
 
-class Charts
+class Charts extends SupChart
 {
-    private $chart;
     private $equipmentsRepository;
     private $occurrencesRepository;
     private $months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-    public function __construct(Chart $chart, EquipmentsRepository $equipmentsRepository, OccurrencesRepository $occurrencesRepository)
+    public function __construct(EquipmentsRepository $equipmentsRepository, OccurrencesRepository $occurrencesRepository)
     {
         $this->equipmentsRepository = $equipmentsRepository;
         $this->occurrencesRepository = $occurrencesRepository;
-        $this->chart = $chart;
     }
 
     public function occurencesChart()
     {
-        $dataset = $this->chart->dataset(trans('occurrences.occurrences'), $this->occurrencesRepository->countByMonths());
-        return $this->chart->createChart(trans('occurrences.occurrences'), 'line', ['width' => 400, 'height' => 100], $this->months, $dataset);
+        $dataset = $this->createDataset(trans('occurrences.occurrences'), $this->occurrencesRepository->countByMonths());
+        return $this->createChart(trans('occurrences.occurrences'), 'line', ['width' => 400, 'height' => 100], $this->months, $dataset);
     }
 
     public function equipmentsChart()
     {
-        $dataset = $this->chart->dataset(trans('inventory.equipments'), $this->equipmentsRepository->countByMonths());
-        return $this->chart->createChart(trans('inventory.equipments'), 'line', ['width' => 400, 'height' => 100], $this->months, $dataset);
+        $dataset = $this->createDataset(trans('inventory.equipments'), $this->equipmentsRepository->countByMonths());
+        return $this->createChart(trans('inventory.equipments'), 'line', ['width' => 400, 'height' => 100], $this->months, $dataset);
     }
 }
--- app/Support/Chart.php
+++ app/Support/Chart.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace InvOco\Services;
+namespace InvOco\Support;
 
-class Chart
+abstract class Chart
 {
-    public function dataset(string $label, array $data, string $bgColor = 'rgba(0, 115, 183, 0.7)', string $borderColor = 'rgba(0, 115, 183, 1)')
+    protected function createDataset(string $label, array $data, string $bgColor = 'rgba(0, 115, 183, 0.7)', string $borderColor = 'rgba(0, 115, 183, 1)')
     {
         return [
             "label" => $label,
@@ -18,7 +18,7 @@ class Chart
         ];
     }
 
-    public function createChart(string $name, string $type, array $size, array $labels, array  $datasets, array $options = [])
+    protected function createChart(string $name, string $type, array $size, array $labels, array  $datasets, array $options = [])
     {
         return app()->chartjs
             ->name($name)
